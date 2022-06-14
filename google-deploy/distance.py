b5f523ed79c250cbb39f15649078ca6d41e138fd
habibiii <adilfaruqhabibi@gmail.com>
2022-06-15 02:40:18 +0700

    Update distance.py

--- google-deploy/distance.py
+++ google-deploy/distance.py
@@ -1,27 +1,20 @@
 import tensorflow as tf
-
-# L1 Distance Layer
-class L1_siamese_dist(tf.keras.layers.Layer):
-    def __init__(self, **kwargs):
-        super().__init__()
-    
-    # Similarity calculation    
-    def call(self, input_embedding, validation_embedding):
-        return abs(input_embedding-validation_embedding)
+from tensorflow.keras.layers import Layer
+from tensorflow.keras import backend as k
 
 # L2 Distance Layer
-class L2_siamese_dist(tf.keras.layers.Layer):
+class L2_siamese_dist(Layer):
   def __init__(self, **kwargs):
       super().__init__()
-  
-  # Similarity calculation    
+
+  # Similarity calculation
   def call(self, input_embedding, validation_embedding):
-    sum_squared = tf.keras.backend.sum(
-        tf.keras.backend.square(input_embedding - validation_embedding),
+    sum_squared = k.sum(
+        k.square(input_embedding - validation_embedding),
         axis=1,
         keepdims=True
     )
-    return tf.keras.backend.sqrt(tf.keras.backend.maximum(
-        sum_squared, 
-        tf.keras.backend.epsilon()
+    return k.sqrt(k.maximum(
+        sum_squared,
+        k.epsilon()
     ))
